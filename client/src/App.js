1d9dc7bc660219790e5066cdeabf772789f73022
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-11 10:27:00 +0100

    import pages to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,16 @@ import './App.css';
 
 import { Route, Routes } from 'react-router-dom';
 
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
 import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
