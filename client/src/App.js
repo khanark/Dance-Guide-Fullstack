0f75b908243c34ddf730fc2e0ecdb83097dd71e0
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-11 22:35:41 +0100

    add new Auth route

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import About from './pages/About/About';
+import Auth from './pages/Auth/Auth';
 import Home from './pages/Home/Home';
-import logo from './logo.svg';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/authentication/:type" element={<Auth />} />
     </Routes>
   );
 }
