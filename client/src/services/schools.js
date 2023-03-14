11d652cca94accd2b5c5f8e6ff8100e785156e41
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-14 21:32:30 +0100

    create getAllSchools in schools service

--- client/src/services/schools.js
+++ client/src/services/schools.js
@@ -3,3 +3,9 @@ import * as api from '../api/api';
 const endpoints = {
   getAll: '/schools',
 };
+
+const getAllSchools = async () => {
+  return api.get(endpoints.getAll);
+};
+
+export { getAllSchools };
