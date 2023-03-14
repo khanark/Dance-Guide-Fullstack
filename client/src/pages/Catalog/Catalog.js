5c9f5744c8ba561f590a3fdeb64aea867edb1380
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-14 21:32:44 +0100

    update catalog page

--- client/src/pages/Catalog/Catalog.js
+++ client/src/pages/Catalog/Catalog.js
@@ -1,11 +1,23 @@
 import './Catalog.scss';
 
+import { useEffect, useState } from 'react';
+
 import Card from '../../components/CardComponent/Card';
 import { FiSearch } from 'react-icons/fi';
 import Layout from '../../components/Layout/Layout';
+import { Link } from 'react-router-dom';
 import PageContainer from '../../components/Layout/PageContainer/PageContainer';
+import { getAllSchools } from '../../services/schools';
 
 const Catalog = () => {
+  const [schools, setSchools] = useState([]);
+
+  useEffect(() => {
+    getAllSchools().then(result => {
+      setSchools(result);
+    });
+  }, []);
+
   return (
     <Layout>
       <PageContainer
@@ -16,16 +28,28 @@ const Catalog = () => {
         }}
       >
         <div className="catalog-page">
-          <div className="search-wrap">
-            <input type="text" name="search" id="seach" placeholder="Търсене" />
-            <span className="search-icon__container">
-              <FiSearch />
-            </span>
-          </div>
+          {schools.length > 0 && (
+            <div className="search-wrap">
+              <input
+                type="text"
+                name="search"
+                id="seach"
+                placeholder="Търсене"
+              />
+              <span className="search-icon__container">
+                <FiSearch />
+              </span>
+            </div>
+          )}
           <div className="card-container">
-            <Card />
-            <Card />
-            <Card />
+            {schools.length === 0 ? (
+              <div className="missing__schools">
+                <p> Все още няма добавени училища. Бъди първият!</p>
+                <Link to="/create">Създай</Link>
+              </div>
+            ) : (
+              schools.map(school => <Card key={school._id} {...school} />)
+            )}
           </div>
         </div>
       </PageContainer>
