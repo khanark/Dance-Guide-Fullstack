e49707c6fafa8f9b8a05cb5fa94688b50a69a002
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-23 22:24:07 +0100

    add the newly created school is being added to the user's created schools array

--- server/services/school_service.js
+++ server/services/school_service.js
@@ -1,11 +1,14 @@
-const DanceSchool = require('../models/DanceSchool');
+const DanceSchool = require("../models/DanceSchool");
+const User = require("../models/User");
 
 const getAllSchools = async () => {
   return DanceSchool.find().lean();
 };
 
 const getSingleSchool = async id => {
-  return DanceSchool.findById(id).populate('owner', ['email', 'firstName', 'lastName']).lean();
+  return DanceSchool.findById(id)
+    .populate("owner", ["email", "firstName", "lastName"])
+    .lean();
 };
 
 const deleteSchool = async id => {
@@ -16,7 +19,15 @@ const updateSchool = async (id, data) => {
   await DanceSchool.findByIdAndUpdate(id, data, { runValidators: true });
 };
 
-const createSchool = async ({ name, image, link, settlementType, settlement, street, ownerId }) => {
+const createSchool = async ({
+  name,
+  image,
+  link,
+  settlementType,
+  settlement,
+  street,
+  ownerId,
+}) => {
   const school = new DanceSchool({
     name,
     image,
@@ -26,6 +37,10 @@ const createSchool = async ({ name, image, link, settlementType, settlement, str
     street,
     owner: ownerId,
   });
+  const user = await User.findById(ownerId);
+  console.log(user);
+  user.danceSchools.push(school);
+  await user.save();
   await school.save();
   return school;
 };
