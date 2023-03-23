787c070a837567a79c324b6485560b66f6894308
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-23 22:14:27 +0100

    u[date userContext component

--- client/src/contexts/userContext.js
+++ client/src/contexts/userContext.js
@@ -1,16 +1,71 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+import userServiceFactory from "../services/users";
 
 export const UserContext = createContext();
 
 const UserContextProvider = ({ children }) => {
   const { user, setUser, clearUser } = useLocalStorage();
+  const [fetchError, setFetchError] = useState(false);
+
+  const { login, register, edit, logout, getSingle } = userServiceFactory(user);
+
+  const navigate = useNavigate();
+
+  const onSubmitLogin = async data => {
+    try {
+      const userData = await login(data);
+      setUser(userData);
+      navigate("/catalog");
+    } catch (error) {
+      setFetchError(true);
+    }
+  };
+
+  const onSubmitRegister = async data => {
+    try {
+      await register(data);
+      navigate("/user/login");
+    } catch (error) {
+      setFetchError(true);
+    }
+  };
+
+  const onSubmitEdit = async data => {
+    try {
+      const userData = await edit(data);
+      setUser(userData);
+      navigate("/catalog");
+    } catch (error) {
+      setFetchError(true);
+    }
+  };
+
+  const onLogout = async () => {
+    clearUser();
+    await logout();
+  };
+
+  const context = {
+    user,
+    fetchError,
+    onSubmitEdit,
+    onSubmitLogin,
+    onSubmitRegister,
+    onLogout,
+    getSingle,
+  };
 
   return (
-    <UserContext.Provider value={{ user, setUser, clearUser }}>
-      {children}
-    </UserContext.Provider>
+    <UserContext.Provider value={context}>{children}</UserContext.Provider>
   );
+};
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  return context;
 };
 
 export default UserContextProvider;
