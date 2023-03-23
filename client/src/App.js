248694e7c4b8570d1bccdfd2318ccdbd9f067b82
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-23 22:14:41 +0100

    update app routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,16 @@
 
 import { Route, Routes } from "react-router-dom";
 
-import Login from "./components/Forms/Login/LoginForm";
-import Edit from "./components/Forms/UserEdit/Edit";
-import Register from "./components/Forms/UserRegister/Register";
 import PrivateRoute from "./contexts/ProtectedRoute";
 import UserContextProvider from "./contexts/UserContext";
 import NotFound from "./pages/404/NotFound";
 import About from "./pages/About/About";
-import User from "./pages/Auth/Auth";
 import Catalog from "./pages/Catalog/Catalog";
 import Create from "./pages/Create/Create";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/LoginForm";
+import Edit from "./pages/UserEdit/Edit";
+import Register from "./pages/UserRegister/Register";
 
 function App() {
   return (
@@ -20,13 +19,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/user" element={<User />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="edit" element={<Edit />} />
-        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/catalog" element={<Catalog />} />
-        <Route element={<PrivateRoute redirectPath="/user/login" />}>
+        <Route element={<PrivateRoute redirectPath="/login" />}>
+          <Route path="/user/edit" element={<Edit />} />
           <Route path="create" element={<Create />} />
         </Route>
         <Route path="*" element={<NotFound />} />
