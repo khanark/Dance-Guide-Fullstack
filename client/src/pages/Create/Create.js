783ff759d13cc7a7ff227ddf1252e096529fff5b
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-24 22:33:18 +0100

    fix createPage

--- client/src/pages/Create/Create.js
+++ client/src/pages/Create/Create.js
@@ -1,15 +1,15 @@
 import "./Create.scss";
 
-import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import FieldsError from "../../components/Forms/Errors/Fields/FieldsError";
 import Layout from "../../components/Layout/Layout";
-import { UserContext } from "../../contexts/UserContext";
+import PageContainer from "../../components/Layout/PageContainer/PageContainer";
+import { useUserContext } from "../../contexts/UserContext";
 import { createSchool } from "../../services/schools";
 
 const Create = () => {
-  const { user } = useContext(UserContext);
+  const { user } = useUserContext();
   const navigate = useNavigate();
 
   const {
@@ -19,6 +19,7 @@ const Create = () => {
   } = useForm();
 
   const linkRegex = /^https?:\/\//;
+  const imageRegex = /(https?:\/\/.*\.(?:jpg|jpeg|png))/;
 
   const onSubmit = async data => {
     try {
@@ -31,109 +32,131 @@ const Create = () => {
 
   return (
     <Layout>
-      <article className="create-page">
-        <div className="form-container">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name">
-              Име
-              <input
-                className="margin-input"
-                {...register("name", {
-                  required: "Моля въведете име",
-                  minLength: {
-                    value: 4,
-                    message: "Името трябва да съдържа най - малко 4 символа",
-                  },
-                })}
-              />
-              <FieldsError msg={errors.name?.message} />
-            </label>
-            <label htmlFor="image">
-              Снимка
-              <input
-                className="margin-input"
-                {...register("image", {
-                  required: "Моля добавете снимка",
-                  pattern: { value: linkRegex, message: "Невалиден URL адрес" },
-                })}
-              />
-              <FieldsError msg={errors.image?.message} />
-            </label>
+      <PageContainer
+        styles={{
+          alignItems: "unset",
+          justifyContent: "unset",
+        }}
+      >
+        <div className="create-page">
+          <div className="form-container">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="name">
+                Име
+                <input
+                  className="margin-input"
+                  {...register("name", {
+                    required: "Моля въведете име",
+                    minLength: {
+                      value: 4,
+                      message: "Името трябва да съдържа най - малко 4 символа",
+                    },
+                    maxLength: {
+                      value: 20,
+                      message: "Името не може да бъде по - дълго от 20 символа",
+                    },
+                  })}
+                />
+                <FieldsError msg={errors.name?.message} />
+              </label>
+              <label htmlFor="image">
+                Снимка
+                <input
+                  className="margin-input"
+                  {...register("image", {
+                    required: "Моля добавете снимка",
+                    pattern: {
+                      value: imageRegex,
+                      message: "Невалиден URL адрес",
+                    },
+                  })}
+                />
+                <FieldsError msg={errors.image?.message} />
+              </label>
 
-            <label htmlFor="link">
-              Официален сайт
-              <input
-                className="margin-input"
-                {...register("link", {
-                  required: "Моля добавете официална страница",
-                  pattern: { value: linkRegex, message: "Невалиден URL адрес" },
-                })}
-              />
-              <FieldsError msg={errors.link?.message} />
-            </label>
-            <label htmlFor="adress">
-              Адрес
-              <div className="address-container">
-                <select
-                  {...register("settlementType")}
-                  className="settlementType"
-                >
-                  <option value="city">Град</option>
-                  <option value="village">Село</option>
-                </select>
-                <div className="settlement-wrapper">
-                  <input
-                    placeholder="населено място"
-                    className="settlement"
-                    {...register("settlement", {
-                      required: "Моля въведете населено място",
-                      minLength: {
-                        value: 3,
-                        message:
-                          "Населеното място трябва да съдържа най - малко 3 символа",
-                      },
-                    })}
-                  />
-                  <FieldsError msg={errors.settlement?.message} />
+              <label htmlFor="link">
+                Официален сайт
+                <input
+                  className="margin-input"
+                  {...register("link", {
+                    required: "Моля добавете официална страница",
+                    pattern: {
+                      value: linkRegex,
+                      message: "Невалиден URL адрес",
+                    },
+                  })}
+                />
+                <FieldsError msg={errors.link?.message} />
+              </label>
+              <label htmlFor="adress">
+                Адрес
+                <div className="address-container">
+                  <select
+                    {...register("settlementType")}
+                    className="settlementType"
+                  >
+                    <option value="city">Град</option>
+                    <option value="village">Село</option>
+                  </select>
+                  <div className="settlement-wrapper">
+                    <input
+                      placeholder="населено място"
+                      className="settlement"
+                      {...register("settlement", {
+                        required: "Моля въведете населено място",
+                        minLength: {
+                          value: 3,
+                          message:
+                            "Населеното място трябва да съдържа най - малко 3 символа",
+                        },
+                        maxLength: {
+                          value: 15,
+                          message:
+                            "Населеното място не може да бъде по - дълго от 15 символа",
+                        },
+                      })}
+                    />
+                    <FieldsError msg={errors.settlement?.message} />
+                  </div>
+                  <div className="street-wrapper">
+                    <input
+                      placeholder="улица"
+                      className="street"
+                      {...register("street", {
+                        required: "Моля въведете улица",
+                        minLength: {
+                          value: 3,
+                          message:
+                            "Улицата трябва да съдържа най - малко 3 символа",
+                        },
+                      })}
+                    />
+                    <FieldsError msg={errors.street?.message} />
+                  </div>
                 </div>
-                <div className="street-wrapper">
-                  <input
-                    placeholder="улица"
-                    className="street"
-                    {...register("street", {
-                      required: "Моля въведете улица",
-                      minLength: {
-                        value: 3,
-                        message:
-                          "Улицата трябва да съдържа най - малко 3 символа",
-                      },
-                    })}
-                  />
-                  <FieldsError msg={errors.street?.message} />
-                </div>
-              </div>
-            </label>
+              </label>
 
-            <button type="submit">Публикувай</button>
-          </form>
-        </div>
-        <div className="side-image">
-          <img
-            id="dance-image"
-            src="https://t3.ftcdn.net/jpg/03/72/71/80/360_F_372718076_pLwDA6fUUDDWvgETtDOr67dUymN8Nzb3.jpg"
-            alt="dance"
-          />
-          <div className="heading">
-            <p>
-              <span className="heading-public">Публикувай</span> своя танцов{" "}
-              <span className="heading-business">бизнес!</span>
-            </p>
-            <p>
-              Нека <span>цяла България</span> научи за това!
-            </p>
+              <button type="submit">Публикувай</button>
+            </form>
+          </div>
+          <div className="side-image">
+            <img
+              id="dance-image"
+              src="https://t3.ftcdn.net/jpg/03/72/71/80/360_F_372718076_pLwDA6fUUDDWvgETtDOr67dUymN8Nzb3.jpg"
+              alt="dance"
+            />
+            <div className="heading">
+              <p>
+                <span className="heading-public">Публикувай</span> своя танцов{" "}
+                <span className="heading-business">бизнес!</span>
+              </p>
+              <p>
+                Нека <span>цяла България</span> научи за това!
+              </p>
+            </div>
           </div>
         </div>
-      </article>
+      </PageContainer>
     </Layout>
   );
 };
