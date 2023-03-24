253db6d05a8a5b281a23c33172a7a4e3d601e99b
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-24 22:33:58 +0100

    update card component

--- client/src/components/CardComponent/Card.js
+++ client/src/components/CardComponent/Card.js
@@ -1,38 +1,35 @@
 // This is what the card should render
 // name, description, link, image, city, street  (owner info)
 
-import './Card.scss';
+import "./Card.scss";
 
-import { IoMdThumbsUp } from 'react-icons/io';
-import { Link } from 'react-router-dom';
-import { MdPinDrop } from 'react-icons/md';
+import { IoMdThumbsUp } from "react-icons/io";
+import { MdPinDrop } from "react-icons/md";
+import { Link } from "react-router-dom";
 
-const Card = () => {
+const Card = ({ _id, name, image, settlement, street, likes }) => {
   return (
     <div className="card-wrapper">
       <div className="card">
-        <Link to={`/details/${'dwada'}`}>
+        <Link to={`/details/${_id}`}>
           <div className="overlay">
             <div className="text">Детайли</div>
           </div>
         </Link>
         <header className="card-header">
-          <img
-            src="https://www.phillymag.com/wp-content/uploads/sites/3/2022/03/powerhouse-dance2-900x600-1.jpg"
-            alt="school"
-          />
+          <img src={image} alt="school" />
         </header>
         <div className="card-body">
           <div className="main-heading">
-            <h3 className="school-name">Andrea dancing school</h3>
+            <h3 className="school-name">{name}</h3>
             <div className="likes">
               <IoMdThumbsUp />
-              <p>230</p>
+              <p>{likes}</p>
             </div>
           </div>
           <div className="adress-wrapper">
             <MdPinDrop />
-            <p>София, Манастирски Ливади 2</p>
+            <p>{settlement}</p>
           </div>
         </div>
       </div>
