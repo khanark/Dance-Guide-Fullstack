87fd190fd61723aa5849b1a517b27e36b45020d7
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-26 21:39:28 +0200

    update school controller

--- server/controllers/schools_controller.js
+++ server/controllers/schools_controller.js
@@ -4,27 +4,55 @@ const {
   createSchool,
   deleteSchool,
   updateSchool,
-} = require('../services/school_service');
-const { handleResponse, validateUtility } = require('../util/responseHandling');
+  likeSchool,
+  unLikeSchool,
+} = require("../services/school_service");
+const { handleResponse, validateUtility } = require("../util/responseHandling");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', handleResponse(getAllSchools));
-
-router.get('/:id', validateUtility('School', { idValidator: true }, 'School'), handleResponse(getSingleSchool));
-
-router.post('/', validateUtility('School', { tokenValidator: true }), handleResponse(createSchool));
-
-router.delete(
-  '/:id',
-  validateUtility({ tokenValidator: true, idValidator: true }, 'School'),
-  handleResponse(deleteSchool, 'School was deleted successfully')
+router.get("/", handleResponse(getAllSchools));
+router.get(
+  "/:id",
+  validateUtility("School", { idValidator: true }, "School"),
+  handleResponse(getSingleSchool)
 );
-
+router.post(
+  "/:id/like",
+  validateUtility(
+    "School",
+    { tokenValidator: true, idValidator: true },
+    "School"
+  ),
+  handleResponse(likeSchool)
+);
+router.post(
+  "/:id/unlike",
+  validateUtility(
+    "School",
+    { tokenValidator: true, idValidator: true },
+    "School"
+  ),
+  handleResponse(unLikeSchool)
+);
+router.post(
+  "/",
+  validateUtility("School", { tokenValidator: true }),
+  handleResponse(createSchool)
+);
+router.delete(
+  "/:id",
+  validateUtility({ tokenValidator: true, idValidator: true }, "School"),
+  handleResponse(deleteSchool, "School was deleted successfully")
+);
 router.put(
-  '/:id',
-  validateUtility('School', { tokenValidator: true, idValidator: true }, 'School'),
-  handleResponse(updateSchool, 'School was updated successfully')
+  "/:id",
+  validateUtility(
+    "School",
+    { tokenValidator: true, idValidator: true },
+    "School"
+  ),
+  handleResponse(updateSchool, "School was updated successfully")
 );
 
 module.exports = router;
