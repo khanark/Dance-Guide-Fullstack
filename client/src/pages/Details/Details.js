e0833ebe515f8219159806ad26a55efb2219d2e9
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-31 22:16:45 +0200

    update Details page

--- client/src/pages/Details/Details.js
+++ client/src/pages/Details/Details.js
@@ -1,23 +1,24 @@
 import "./Details.scss";
 
 import { useEffect, useState } from "react";
-import { BsCalendarDate, BsFillTelephoneFill } from "react-icons/bs";
-import { Link, useParams } from "react-router-dom";
 
-import { BsFillHouseFill } from "react-icons/bs";
-import { GrFormNextLink } from "react-icons/gr";
-import { MdEmail } from "react-icons/md";
+import { useParams } from "react-router-dom";
 import defaultAvatar from "../../assets/images/blank-avatar-image.jpg";
 import Layout from "../../components/Layout/Layout";
 import PageContainer from "../../components/Layout/PageContainer/PageContainer";
 import { useUserContext } from "../../contexts/UserContext";
 import schoolsFactory from "../../services/schools";
-import { formatDate } from "../../util/util";
+import Adress from "./components/Adress";
+import Contacts from "./components/Contacts";
+import Heading from "./components/Heading";
+import LikeButton from "./components/LikeButton";
+import UserButtons from "./components/UserButtons";
 
 const Details = () => {
-  const [school, setSchool] = useState({});
-  const [isLiked, setIsLiked] = useState(() => {
-    return school.likes?.users.includes(user._id);
+  const [school, setSchool] = useState({
+    schoolDetails: {},
+    isOwner: false,
+    isLiked: false,
   });
 
   const { user } = useUserContext();
@@ -26,23 +27,26 @@ const Details = () => {
 
   useEffect(() => {
     singleSchool(schoolId).then(data => {
-      setSchool(data);
+      setSchool({
+        schoolDetails: data,
+        isLiked: data.likes.users.includes(user?._id),
+        isOwner: data.owner?._id == user?._id,
+      });
     });
   }, []);
 
-  const handleLikes = async () => {
-    let school;
-    if (isLiked) {
-      school = await unLikeSchool(schoolId);
-      setIsLiked(false);
-    } else {
-      school = await likeSchool(schoolId);
-      setIsLiked(true);
-    }
-    setSchool(Object.assign({}, school));
-  };
+  const { schoolDetails, isLiked, isOwner } = school;
+  console.log(schoolDetails);
 
-  console.log(school);
+  const handleLikes = async () => {
+    if (school.isLiked) {
+      await unLikeSchool(schoolId);
+      setSchool({ ...school, isLiked: false });
+    } else {
+      await likeSchool(schoolId);
+      setSchool({ ...school, isLiked: true });
+    }
+  };
 
   return (
     <Layout>
@@ -54,50 +58,27 @@ const Details = () => {
       >
         <div className="details-page">
           <div className="details-page__image">
-            <img src={school.image} alt="" />
+            <img src={schoolDetails.image} alt="" />
           </div>
           <div className="details-page__info">
-            <div className="heading__info">
-              <h2 className="school__name">{school.name}</h2>
-              <div className="created-at">
-                <BsCalendarDate />
-                <p>{formatDate(school._createdAt)}</p>
-              </div>
+            {school.isOwner && <UserButtons id={schoolId} />}
+            <div className="details-wrapper">
+              <Heading {...schoolDetails} />
+              <Adress {...schoolDetails} />
+              <div className="horizontal__line"></div>
+              <Contacts {...schoolDetails} />
             </div>
-            <div className="address">
-              <h3 className="address__title">Адрес</h3>
-              <div className="address__info">
-                <BsFillHouseFill />
-                <p>
-                  {school.settlement}, улица: {school.street}
-                </p>
-              </div>
-            </div>
-            <div className="contacts">
-              <h3 className="contacts__title">Контакти</h3>
-              <div className="contacts__info">
-                <div className="phone">
-                  <BsFillTelephoneFill />
-                  <p>{school.owner?.phoneNumber}</p>
-                </div>
-                <div className="split__line"></div>
-                <div className="email">
-                  <MdEmail />
-                  <p>{school.owner?.email}</p>
-                </div>
-                <div className="split__line"></div>
-                <div className="link">
-                  <GrFormNextLink />
-                  <Link className="page__link" to={school.link} type="_blank">
-                    Официална страница
-                  </Link>
-                </div>
-              </div>
-            </div>
+            {!isOwner && user && (
+              <LikeButton handleLikes={handleLikes} isLiked={isLiked} />
+            )}
           </div>
           <div className="avatar__container">
             <img
-              src={!school.owner?.avatar ? defaultAvatar : school.owner?.avatar}
+              src={
+                !schoolDetails.owner?.avatar
+                  ? defaultAvatar
+                  : schoolDetails.owner?.avatar
+              }
               alt="owner"
             />
           </div>
