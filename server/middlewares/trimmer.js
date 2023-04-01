db5c19106f628736aa233c774ee7efc60395cfe0
JScodeKiddie <JScodeKiddie@gmail.com>
2023-04-01 15:27:18 +0200

    fix trimmer working with objects

--- server/middlewares/trimmer.js
+++ server/middlewares/trimmer.js
@@ -2,6 +2,9 @@ module.exports = () => {
   return (req, res, next) => {
     if (req.body) {
       for (const key in req.body) {
+        if(typeof req.body[key] !== "string") {
+          continue
+        }
         req.body[key] = req.body[key].trim();
       }
     }
