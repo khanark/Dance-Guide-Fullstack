216792f332844306e94fc5dcca0f52e7f0719fef
JScodeKiddie <JScodeKiddie@gmail.com>
2023-04-07 10:46:43 +0200

    fix date [broken parsing]

--- client/src/pages/Details/components/Heading.js
+++ client/src/pages/Details/components/Heading.js
@@ -2,14 +2,14 @@ import { danceTypeFormat, formatDate } from "../../../util/util";
 
 import { BsCalendarDate } from "react-icons/bs";
 
-const Heading = ({ name, _createdAt, schoolType }) => {
+const Heading = ({ name, createdAt, schoolType }) => {
   return (
     <div className="heading__info">
       <h2 className="school__name">{name}</h2>
       <p className="school__type">Вид танци: {danceTypeFormat(schoolType)} </p>
       <div className="created-at">
         <BsCalendarDate />
-        <p>{formatDate(_createdAt)}</p>
+        <p>{formatDate(createdAt)}</p>
       </div>
     </div>
   );
--- client/src/util/util.js
+++ client/src/util/util.js
@@ -1,6 +1,6 @@
 export const formatDate = (val) => {
   const date = new Date(val);
-  return date.toLocaleDateString("bg-BG", {
+  return date.toLocaleString("bg-BG", {
     day: "numeric",
     month: "long",
     year: "2-digit",
