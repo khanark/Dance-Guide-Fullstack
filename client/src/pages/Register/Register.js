f8c5949d31ff959c9794bcc8d84dc7c8f433e79f
JScodeKiddie <JScodeKiddie@gmail.com>
2023-06-22 17:54:06 +0200

    update pageHeader component styles

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -4,7 +4,6 @@ import './Register.css';
 import { useEffect, useState } from 'react';
 
 import FieldsError from '../../components/Forms/Errors/Fields/FieldsError';
-import Layout from '../../components/Layout/Layout';
 import { Spinner } from '@chakra-ui/react';
 import { registerSchemaValidation } from '../../YupSchemas/validation_schema';
 import { registerUser } from '../../services/users';
